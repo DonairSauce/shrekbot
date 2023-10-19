157f8c2f31ac384c106ac40d9ceea5f626cd28bd
ShrekSauce <andre_pelland@hotmail.com>
2023-10-19 13:18:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,74 +1,57 @@
-console.log('Initializing shrekbot');
+// Required modules and initializations
 const fs = require('node:fs');
 const path = require('node:path');
-const {Client, Collection, GatewayIntentBits} = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const express = require('express');
 const request = require('./commands/request.js');
-const {token} = process.env;
+const { token } = process.env;
 const channelFeed = process.env.channelfeed;
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
-require('child_process').fork('deploy-commands.js');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// Load commands
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+    const command = require(path.join(commandsPath, file));
     client.commands.set(command.data.name, command);
 }
 
-console.log('Command files loaded');
-
 let currentId = 0;
+const generateId = () => {
+    currentId++;
+    return currentId;
+};
 
+// Client events
 client.once('ready', () => {
     const pjson = require('./package.json');
-    console.log(pjson);
-    const ombiIP = process.env.ombiip;
-    const ombiPort = process.env.ombiport;
-    const timerExp = process.env.timerexp;
-    console.log('Using ombi: ' + ombiIP + ':' + ombiPort);
-    console.log('Message timeout set to: ' + timerExp);
+    console.log(`Bot version: ${pjson.version}`);
+    console.log(`Using ombi: ${process.env.ombiip}:${process.env.ombiport}`);
+    console.log(`Message timeout set to: ${process.env.timerexp}`);
 });
 
 client.on('interactionCreate', async interaction => {
-    function generateId() {
-        currentId++;
-        return currentId;
-    }
-
     if (interaction.isCommand()) {
-        await interaction.deferReply({ content: 'Processing your request...' });
-        
         const command = client.commands.get(interaction.commandName);
-
         if (!command) return;
-
         try {
             await command.execute(interaction, generateId());
         } catch (error) {
             console.error(error);
-            await interaction.editReply('There was an error while executing this command!');
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
-    } 
-    else if (interaction.isStringSelectMenu()) {
-        if (interaction.customId === 'media_selector') {
-            request.search(interaction.values[0], interaction);
-        }
-    } 
-    else if (interaction.isButton()) {
-        if (interaction.customId.includes('-button')) {
-            if (interaction.customId.includes('request')) {
-                try {
-                    const id = interaction.customId.split('-')[2];
-                    const mediaType = interaction.customId.split('-')[3];
-                    const messageId = interaction.customId.split('-')[4];
-                    interaction.deferUpdate();
-                    await request.sendRequest(interaction, id, mediaType, messageId);
-                } catch (error) {
-                    console.log(error);
-                }
+    } else if (interaction.isStringSelectMenu() && interaction.customId === 'media_selector') {
+        request.search(interaction.values[0], interaction);
+    } else if (interaction.isButton() && interaction.customId.includes('-button')) {
+        if (interaction.customId.includes('request')) {
+            try {
+                const [_, __, id, mediaType, messageId] = interaction.customId.split('-');
+                interaction.deferUpdate();
+                await request.sendRequest(interaction, id, mediaType, messageId);
+            } catch (error) {
+                console.log(error);
             }
         }
     }
@@ -76,33 +59,17 @@ client.on('interactionCreate', async interaction => {
 
 client.login(token);
 
-// Express section
-const express = require('express');
+// Express server for webhook
 const app = express();
-
 app.use(express.json());
-
 app.post('/webhook', (req, res) => {
-    const payload = req.body;
-
-    // Extract relevant data from the payload
-    const {requestedByAlias, title, userName, requestStatus} = payload;
+    const { requestedByAlias, title, userName, requestStatus } = req.body;
     if (requestStatus === 'Available') {
-        let userId = '';
-        if (requestedByAlias) {
-            if (requestedByAlias.includes(',')) {
-                userId = '<@' + requestedByAlias.split(',')[1] + '>';
-            } else {
-                userId = requestedByAlias;
-            }
-        } else {
-            userId = userName;
-        }
+        const userId = requestedByAlias?.includes(',')
+            ? `<@${requestedByAlias.split(',')[1]}>`
+            : userName || requestedByAlias;
 
-        // Compose the Discord webhook message
-        const discordMessage = `${userId}, ${title} is now available!`;
-        client.channels.cache.get(channelFeed).send(discordMessage);
-
+        client.channels.cache.get(channelFeed).send(`${userId}, ${title} is now available!`);
         res.sendStatus(200);
     }
 });
