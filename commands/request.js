2da45c8ec4da230ce13d002f352f559e6f567241
Fire-Swan <philippedupuis7@gmail.com>
2025-03-18 23:56:11 -0400

    Clean up code structure and formatting across bot files

--- commands/request.js
+++ commands/request.js
@@ -1,15 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { ActionRowBuilder, StringSelectMenuBuilder, ButtonBuilder, Collection } = require('discord.js');
-const { ButtonStyle } = require('discord.js');
+const {
+	ActionRowBuilder,
+	StringSelectMenuBuilder,
+	ButtonBuilder,
+	Collection,
+	ButtonStyle,
+} = require('discord.js');
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
+
 const ombiIP = process.env.ombiip;
 const ombiPort = process.env.ombiport;
 const ombiToken = process.env.ombitoken;
 const timerExp = process.env.timerexp;
+
 let objectsWithoutDefault = [];
 const timerManager = new Collection();
-
 const seasonSelections = new Map();
 
 module.exports = {
@@ -21,30 +27,24 @@ module.exports = {
 			option.setName('search')
 				.setDescription('Enter the name of a TV show or movie.')
 				.setMaxLength(75)
-				.setRequired(true)),
+				.setRequired(true)
+		),
 	async execute(interaction, messageId) {
-		// Search term
-		let args = interaction.options.getString('search');
-		args = args.toString();
-		args = args.replace(/,/g, ' ');
+		let args = interaction.options.getString('search').toString().replace(/,/g, ' ');
 		const query = encodeURIComponent(args);
 		const idOfFirstItem = await this.getSearchResults(interaction, messageId, query);
 		if (idOfFirstItem !== undefined) {
 			this.search(idOfFirstItem, interaction);
 		}
-
-		console.log('Search for "' + args + '" by ' + interaction.member.user.username + ' done');
+		console.log(`Search for "${args}" by ${interaction.member.user.username} done`);
 	},
 	async getSearchResults(interaction, messageId, args) {
 		const searchTerm = decodeURIComponent(args);
-		console.log(interaction.member.user.username + ' searched for "' + searchTerm + '"');
-		// Api call
+		console.log(`${interaction.member.user.username} searched for "${searchTerm}"`);
+		const body = { movies: true, tvShows: true, music: false, people: false };
 		let searchResults = {};
-		const body = {
-			movies: true, tvShows: true, music: false, people: false,
-		};
 		try {
-			searchResults = await fetch('http://' + ombiIP + ':' + ombiPort + '/api/v2/Search/multi/' + args, {
+			searchResults = await fetch(`http://${ombiIP}:${ombiPort}/api/v2/Search/multi/${args}`, {
 				method: 'post',
 				body: JSON.stringify(body),
 				headers: {
@@ -52,147 +52,92 @@ module.exports = {
 					ApiKey: ombiToken,
 					'Content-Type': 'application/json-patch+json',
 				},
-
 			}).then(response => response.json());
 		} catch (err) {
 			console.log(err);
 		}
 
-		// Place results into an object
 		if (Object.keys(searchResults).length > 0) {
 			const mediaResults = [];
-			let maxResults = 10;
-
-			if (Object.keys(searchResults).length < maxResults) {
-				maxResults = Object.keys(searchResults).length;
-			}
-
+			const maxResults = Math.min(10, Object.keys(searchResults).length);
 			for (let i = 0; i < maxResults; i++) {
-				const object = {};
-
-				object.id = searchResults[i].id;
-
-				object.mediaType = searchResults[i].mediaType;
-				object.title = searchResults[i].title;
-				object.overview = searchResults[i].overview;
-				object.poster = searchResults[i].poster;
-				mediaResults.push(object);
-			}
-
-			// Reply with dropdown selection
-			let idOfFirstItem = '';
-			idOfFirstItem = `${mediaResults[0].mediaType + ',' + mediaResults[0].id + ',' + messageId + ',' + args}`;
-
-			objectsWithoutDefault = [];
-			mediaResults.forEach(o => {
-				const emoji = o.mediaType === 'movie' ? '🎥' : '📺';
-				objectsWithoutDefault.push({
-					label: `${o.title.substr(0, 97) + '...'}`,
-					description: `${o.overview.substr(0, 97) + '...'}`,
-					value: `${o.mediaType + ',' + o.id + ',' + messageId + ',' + args}`,
-					emoji,
+				const o = searchResults[i];
+				mediaResults.push({
+					label: o.title.length > 97 ? o.title.substr(0, 97) + '...' : o.title,
+					description: o.overview.length > 97 ? o.overview.substr(0, 97) + '...' : o.overview,
+					value: `${o.mediaType},${o.id},${messageId},${args}`,
+					emoji: o.mediaType === 'movie' ? '🎥' : '📺',
 				});
-			});
-
-			if (idOfFirstItem) {
-				return idOfFirstItem;
 			}
+			objectsWithoutDefault = mediaResults;
+			return mediaResults[0].value;
 		} else {
+			console.log(`No results found for "${searchTerm}"`);
 			try {
-				console.log('No results found for "' + searchTerm + '"');
-				interaction.reply({ content: 'No results available for: "' + searchTerm + '". Please try searching again.', ephemeral: true });
+				interaction.reply({ content: `No results available for: "${searchTerm}". Please try again.`, ephemeral: true });
 			} catch (err) {
 				console.log(err);
 			}
 		}
 	},
-
 	async search(id, interaction) {
-		const splitArray = id.split(',');
-		const mediaType = splitArray[0];
-		const movieDbId = splitArray[1];
-		const messageId = splitArray[2];
-		const searchQuery = splitArray[3];
+		const [mediaType, movieDbId, messageId, searchQuery] = id.split(',');
 		await this.getSearchResults(interaction, messageId, searchQuery);
-
-		let isMovie = false;
-		let isTv = false;
-		let apiSubUrl;
-
-		if (mediaType === 'movie') {
-			isMovie = true;
-			apiSubUrl = '/api/v2/Search/movie/';
-		}
-
-		if (mediaType === 'tv') {
-			isTv = true;
-			apiSubUrl = '/api/v2/Search/tv/moviedb/';
-		}
-
+		const isMovie = mediaType === 'movie';
+		const isTv = mediaType === 'tv';
+		const apiSubUrl = isMovie ? '/api/v2/Search/movie/' : '/api/v2/Search/tv/moviedb/';
 		let info;
-
 		try {
-			info = await fetch('http://' + ombiIP + ':' + ombiPort + apiSubUrl + movieDbId, {
+			info = await fetch(`http://${ombiIP}:${ombiPort}${apiSubUrl}${movieDbId}`, {
 				method: 'get',
-				headers: {
-					accept: 'application/json',
-					ApiKey: ombiToken,
-				},
+				headers: { accept: 'application/json', ApiKey: ombiToken },
 			}).then(response => response.json());
 		} catch (err) {
 			console.log(err);
 		}
 
-		// Build an object with the TV/movie info
 		const object = {
 			id: info.id,
-			releaseDate: (mediaType === 'movie' ? info.releaseDate : info.firstAired),
+			releaseDate: isMovie ? info.releaseDate : info.firstAired,
 			title: info.title,
 			description: info.overview,
-			image: (mediaType === 'movie' ? info.posterPath : info.banner),
+			image: isMovie ? info.posterPath : info.banner,
 			imdbID: info.imdbId,
 			available: info.available,
 			requested: info.requested,
 		};
 
-		// Embed builder
 		function showBuilder() {
 			try {
 				const embed = new Discord.EmbedBuilder()
 					.setColor('#0099ff')
-					.setTitle(object.title + (object.releaseDate === null ? '' : (' (' + object.releaseDate.substring(0, 4) + ')')))
-					.setURL('https://imdb.com/title/' + object.imdbID)
+					.setTitle(object.title + (object.releaseDate ? ` (${object.releaseDate.substring(0, 4)})` : ''))
+					.setURL(`https://imdb.com/title/${object.imdbID}`)
 					.setDescription(object.description ? object.description.substr(0, 255) + '(...)' : 'No description')
-					.setImage('https://image.tmdb.org/t/p/original/' + object.image)
+					.setImage(`https://image.tmdb.org/t/p/original/${object.image}`)
 					.setTimestamp()
 					.setFooter({
-						text: 'Searched by ' + interaction.member.user.username,
+						text: `Searched by ${interaction.member.user.username}`,
 						iconURL: `https://cdn.discordapp.com/avatars/${interaction.member.user.id}/${interaction.member.user.avatar}.png`,
 					});
-
 				if (object.available) {
 					embed.addFields([{ name: '__Available__', value: '✅', inline: true }]);
 				}
-
 				if (object.requested) {
 					embed.addFields([{ name: '__Requested__', value: '✅', inline: true }]);
 				}
-
 				return embed;
 			} catch (err) {
-				console.log('error in showBuilder: ' + err);
+				console.log('error in showBuilder:', err);
 			}
 		}
 
-		// Timer for auto cleanup
 		function timeOut(interaction) {
-			if (timerManager.has(messageId)) {
-				clearTimeout(timerManager.get(messageId));
-			}
+			if (timerManager.has(messageId)) clearTimeout(timerManager.get(messageId));
 			const timer = setTimeout(() => {
 				try {
-					console.log('Search for ' + info.title + ' timed out');
-					interaction.followUp({ content: 'Your request for ' + info.title + ' timed out', ephemeral: true });
+					console.log(`Search for ${info.title} timed out`);
+					interaction.followUp({ content: `Your request for ${info.title} timed out`, ephemeral: true });
 					interaction.deleteReply();
 				} catch (err) {
 					console.log(err);
@@ -203,73 +148,60 @@ module.exports = {
 
 		const embedMessage = showBuilder();
 
-		// Build the dropdown for making another selection
 		const objectSelect = new StringSelectMenuBuilder()
 			.setCustomId('media_selector')
 			.setPlaceholder('Make another selection')
 			.addOptions(objectsWithoutDefault);
 		const selectMenu = new ActionRowBuilder().addComponents(objectSelect);
-
-		// Build the overall components array starting with the select menu
 		let componentsArray = [selectMenu];
 
-		// For TV shows, add a season selection dropdown
 		if (isTv) {
-			// Build season options: first option is "All Seasons" with default: true
 			let seasonOptions = [{
 				label: 'All Seasons',
 				description: 'Request every season of the show',
 				value: 'all',
-				default: true
+				default: true,
 			}];
-
-			// If the API returned seasonRequests, list each season as an option.
 			if (info.seasonRequests && Array.isArray(info.seasonRequests)) {
 				info.seasonRequests.forEach(seasonObj => {
 					if (typeof seasonObj.seasonNumber !== 'undefined') {
 						seasonOptions.push({
 							label: `Season ${seasonObj.seasonNumber}`,
 							description: `Request Season ${seasonObj.seasonNumber}`,
-							value: seasonObj.seasonNumber.toString()
+							value: seasonObj.seasonNumber.toString(),
 						});
 					}
 				});
 			}
-
 			const seasonSelect = new StringSelectMenuBuilder()
-				.setCustomId('season_selector-' + messageId)
+				.setCustomId(`season_selector-${messageId}`)
 				.setPlaceholder('Select season option')
 				.addOptions(seasonOptions);
-
 			const seasonRow = new ActionRowBuilder().addComponents(seasonSelect);
 			componentsArray.push(seasonRow);
 		}
 
-		// Add either the Request button or a disabled button if already requested/available
 		if (!(object.requested || object.available)) {
-			// Not requested/available: add the Request button
 			const requestRow = new ActionRowBuilder().addComponents(
 				new ButtonBuilder()
-					.setCustomId('request-button-' + movieDbId + '-' + mediaType + '-' + messageId)
+					.setCustomId(`request-button-${movieDbId}-${mediaType}-${messageId}`)
 					.setStyle(ButtonStyle.Primary)
-					.setLabel('Request'),
+					.setLabel('Request')
 			);
 			componentsArray.push(requestRow);
 		} else {
-			// Already requested/available: add a disabled button
 			const disabledRow = new ActionRowBuilder().addComponents(
 				new ButtonBuilder()
 					.setCustomId('mediaAvailable')
-					.setLabel(object.title.substr(0, 58) + ' Is Already ' + (object.available ? 'Available' : 'Requested') + '!')
+					.setLabel(`${object.title.substr(0, 58)} Is Already ${object.available ? 'Available' : 'Requested'}!`)
 					.setStyle(ButtonStyle.Primary)
-					.setDisabled(true),
+					.setDisabled(true)
 			);
 			componentsArray.push(disabledRow);
 		}
 
-		// Send reply/update depending on the interaction state.
 		try {
-			if (interaction.message === undefined) {
+			if (!interaction.message) {
 				interaction.reply({ embeds: [embedMessage], components: componentsArray }).then(() => {
 					timeOut(interaction);
 				});
@@ -282,32 +214,28 @@ module.exports = {
 			console.log(err);
 		}
 	},
-
 	async sendRequest(interaction, id, mediaType, messageId, seasonSelection) {
 		const processing = new ActionRowBuilder().addComponents(
 			new ButtonBuilder()
 				.setCustomId('processing')
 				.setStyle(ButtonStyle.Success)
 				.setLabel('Your request is processing')
-				.setDisabled(true),
+				.setDisabled(true)
 		);
-
-		await interaction.message.edit({
-			components: [processing],
-		});
+		await interaction.message.edit({ components: [processing] });
 		clearTimeout(timerManager.get(messageId));
 		const { member } = interaction;
-		console.log(member.user.username + ' sent a request to Ombi');
+		console.log(`${member.user.username} sent a request to Ombi`);
 
 		if (mediaType === 'movie') {
 			try {
-				fetch('http://' + ombiIP + ':' + ombiPort + '/api/v1/Request/movie', {
+				fetch(`http://${ombiIP}:${ombiPort}/api/v1/Request/movie`, {
 					method: 'post',
 					headers: {
 						Accept: 'application/json',
 						'Content-Type': 'text/json',
 						ApiKey: ombiToken,
-						ApiAlias: member.user.username + '#' + member.user.discriminator + ',' + member.user.id,
+						ApiAlias: `${member.user.username}#${member.user.discriminator},${member.user.id}`,
 					},
 					body: JSON.stringify({
 						theMovieDbId: id,
@@ -328,36 +256,26 @@ module.exports = {
 				console.error(err);
 			}
 		} else if (mediaType === 'tv') {
-			// Build the request body based on the season selection.
-			let requestBody = {
-				theMovieDbId: id,
-				languageCode: 'en',
-			};
-
-			// If no specific season was stored or the selection is "all", request all seasons.
+			let requestBody = { theMovieDbId: id, languageCode: 'en' };
 			if (!seasonSelection || seasonSelection === 'all') {
 				requestBody.requestAll = true;
 			} else {
-				// If a specific season number is provided:
 				requestBody.requestAll = false;
 				requestBody.latestSeason = false;
 				requestBody.firstSeason = false;
-				requestBody.seasons = [
-					{
-						seasonNumber: parseInt(seasonSelection, 10),
-						episodes: [] // Request all episodes of that season.
-					}
-				];
+				requestBody.seasons = [{
+					seasonNumber: parseInt(seasonSelection, 10),
+					episodes: []
+				}];
 			}
-
 			try {
-				fetch('http://' + ombiIP + ':' + ombiPort + '/api/v2/Requests/tv', {
+				fetch(`http://${ombiIP}:${ombiPort}/api/v2/Requests/tv`, {
 					method: 'post',
 					headers: {
 						Accept: 'application/json',
 						'Content-Type': 'application/json',
 						ApiKey: ombiToken,
-						ApiAlias: member.user.username + '#' + member.user.discriminator + ',' + member.user.id,
+						ApiAlias: `${member.user.username}#${member.user.discriminator},${member.user.id}`,
 					},
 					body: JSON.stringify(requestBody),
 				})
@@ -375,24 +293,18 @@ module.exports = {
 				console.error(err);
 			}
 		}
-
 		async function changeButton(responseStatusCode, jsonResponse) {
 			console.log(jsonResponse);
-			let success = false;
-			console.log('response code ' + responseStatusCode);
-			success = responseStatusCode >= 200 && responseStatusCode < 300;
+			const success = responseStatusCode >= 200 && responseStatusCode < 300;
 			const row = new ActionRowBuilder().addComponents(
 				new ButtonBuilder()
-					.setCustomId('request-sent-button-' + id + '-' + mediaType + '-' + messageId)
+					.setCustomId(`request-sent-button-${id}-${mediaType}-${messageId}`)
 					.setStyle(success && !jsonResponse.isError ? ButtonStyle.Success : ButtonStyle.Danger)
-					.setLabel(success && !jsonResponse.isError ? 'Your request has been submitted' : 'Request Failed: Error ' + responseStatusCode)
-					.setDisabled(true),
+					.setLabel(success && !jsonResponse.isError ? 'Your request has been submitted' : `Request Failed: Error ${responseStatusCode}`)
+					.setDisabled(true)
 			);
-			await interaction.message.edit({
-				components: [row],
-			});
+			await interaction.message.edit({ components: [row] });
 		}
-
 		console.log('All done, now get out of my swamp');
 	}
 };
--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 console.log('Initializing shrekbot');
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle, InteractionType, ActionRowBuilder } = require('discord.js');
+const {
+	Client,
+	Collection,
+	GatewayIntentBits,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+	InteractionType,
+	ActionRowBuilder,
+} = require('discord.js');
 
 const request = require('./commands/request.js');
 const { token } = process.env;
 const channelFeed = process.env.channelfeed;
+
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 require('child_process').fork('deploy-commands.js');
+
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -23,17 +34,13 @@ let currentId = 0;
 client.once('ready', () => {
 	const pjson = require('./package.json');
 	console.log(pjson);
-	const ombiIP = process.env.ombiip;
-	const ombiPort = process.env.ombiport;
-	const timerExp = process.env.timerexp;
-	console.log('Using ombi: ' + ombiIP + ':' + ombiPort);
-	console.log('Message timeout set to: ' + timerExp);
+	console.log(`Using ombi: ${process.env.ombiip}:${process.env.ombiport}`);
+	console.log(`Message timeout set to: ${process.env.timerexp}`);
 });
 
 client.on('interactionCreate', async interaction => {
 	function generateId() {
-		currentId++;
-		return currentId;
+		return ++currentId;
 	}
 
 	if (interaction.isCommand()) {
@@ -46,19 +53,17 @@ client.on('interactionCreate', async interaction => {
 			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 		}
 	} else if (interaction.isStringSelectMenu()) {
-		// Handle the media selection
+		// Media selection
 		if (interaction.customId === 'media_selector') {
 			request.search(interaction.values[0], interaction);
 		}
-		// Handle the season selection dropdown. Expect customId in the format "season_selector-<messageId>"
+		// Season selection; expected format: "season_selector-<messageId>"
 		else if (interaction.customId.startsWith('season_selector-')) {
-			const parts = interaction.customId.split('-');
-			const messageId = parts[1]; // e.g., "season_selector-<messageId>"
-			const seasonChoice = interaction.values[0]; // "all" or "specific"
+			const [, messageId] = interaction.customId.split('-');
+			const seasonChoice = interaction.values[0];
 			if (seasonChoice === 'specific') {
-				// Create and show a modal for season input.
 				const modal = new ModalBuilder()
-					.setCustomId('seasonModal-' + messageId)
+					.setCustomId(`seasonModal-${messageId}`)
 					.setTitle('Enter Season Number');
 				const seasonInput = new TextInputBuilder()
 					.setCustomId('seasonNumber')
@@ -70,37 +75,29 @@ client.on('interactionCreate', async interaction => {
 				modal.addComponents(firstActionRow);
 				await interaction.showModal(modal);
 			} else {
-				// If "all" is selected, store that selection.
 				request.seasonSelections.set(messageId, 'all');
 				await interaction.reply({ content: 'You selected all seasons.', ephemeral: true });
 			}
 		}
 	} else if (interaction.type === InteractionType.ModalSubmit) {
-		// Handle modal submissions for season input.
 		if (interaction.customId.startsWith('seasonModal-')) {
-			const parts = interaction.customId.split('-');
-			// The customId is in the format "seasonModal-<messageId>"
-			const messageId = parts[1];
+			const [, messageId] = interaction.customId.split('-');
 			const seasonNumber = interaction.fields.getTextInputValue('seasonNumber');
-			// Store the season number selection.
 			request.seasonSelections.set(messageId, seasonNumber);
 			await interaction.reply({ content: `You selected season ${seasonNumber}.`, ephemeral: true });
 		}
 	} else if (interaction.isButton()) {
-		if (interaction.customId.includes('-button')) {
-			if (interaction.customId.includes('request')) {
-				try {
-					const parts = interaction.customId.split('-');
-					const id = parts[2];
-					const mediaType = parts[3];
-					const messageId = parts[4];
-					interaction.deferUpdate();
-					// Retrieve the stored season selection (default to 'all' if not set)
-					const seasonSelection = request.seasonSelections.get(messageId) || 'all';
-					await request.sendRequest(interaction, id, mediaType, messageId, seasonSelection);
-				} catch (error) {
-					console.log(error);
-				}
+		if (interaction.customId.includes('-button') && interaction.customId.includes('request')) {
+			try {
+				const parts = interaction.customId.split('-');
+				const id = parts[2];
+				const mediaType = parts[3];
+				const messageId = parts[4];
+				interaction.deferUpdate();
+				const seasonSelection = request.seasonSelections.get(messageId) || 'all';
+				await request.sendRequest(interaction, id, mediaType, messageId, seasonSelection);
+			} catch (error) {
+				console.error(error);
 			}
 		}
 	}
@@ -108,48 +105,29 @@ client.on('interactionCreate', async interaction => {
 
 client.login(token);
 
-// Express section
+// Express server
 const express = require('express');
 const app = express();
-
 app.use(express.json());
 
 app.post('/webhook', (req, res) => {
 	console.log('Received webhook:', req.body);
-	const payload = req.body;
-
-	try {
-		// Extract relevant data from the payload
-		const { requestedByAlias, title, userName, requestStatus } = payload;
-
-		if (requestStatus === 'Available') {
-			let userId = '';
-			if (requestedByAlias) {
-				if (requestedByAlias.includes(',')) {
-					userId = '<@' + requestedByAlias.split(',')[1] + '>';
-				} else {
-					userId = requestedByAlias;
-				}
-			} else {
-				userId = userName;
-			}
-
-			// Compose the Discord webhook message
-			const discordMessage = `${userId}, ${title} is now available!`;
-			client.channels.cache.get(channelFeed).send(discordMessage);
-
-			res.sendStatus(200);
-		} else {
-			console.log(`Request status is not 'Available', it is: '${requestStatus}'`);
-			res.sendStatus(200); // You might want to send a different status if the request is not processed
-		}
-	} catch (error) {
-		console.error('Error processing webhook:', error);
-		res.status(500).send('Internal Server Error');
+	const { requestedByAlias, title, userName, requestStatus } = req.body;
+	if (requestStatus === 'Available') {
+		let userId = requestedByAlias
+			? requestedByAlias.includes(',')
+				? `<@${requestedByAlias.split(',')[1]}>`
+				: requestedByAlias
+			: userName;
+		const discordMessage = `${userId}, ${title} is now available!`;
+		client.channels.cache.get(channelFeed).send(discordMessage);
+		res.sendStatus(200);
+	} else {
+		console.log(`Request status is not 'Available', it is: '${requestStatus}'`);
+		res.sendStatus(200);
 	}
 });
 
 app.listen(8154, () => {
 	console.log('Webhook server is running on port 8154');
 });
-
