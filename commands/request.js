6f9e6cc796a6042dd3ea2399c91d79f7c4e305f8
Fire-Swan <philippedupuis7@gmail.com>
2025-03-30 21:07:18 -0400

    add logs

--- commands/request.js
+++ commands/request.js
@@ -242,7 +242,9 @@ module.exports = {
 					});
 
 				if (isTv) {
-					const allRequested = totalSeasons > 0 && remaining.length === 0;	
+					const allRequested = totalSeasons > 0 && remaining.length === 0;
+					console.log(`All requested ${allRequested}`)	
+					console.log(`Show is available: ${object.available}`);
 					console.log(`Remaining season(s) length: ${remaining.length}`);
 					const noneRequested = requestedSeasons.length === 0;
 
